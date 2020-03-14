1a50d8a0c8227ae47f0a146b43617f7212ad05bb
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-14 14:48:05 -0400

    Added an api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js")
 
-router.get("/api/workouts")
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbWOrkout =>{
+      res.json(dbWOrkout)
+    })
+    .catch( err => {
+      res.status(400).json(err)
+    })
+})
+
+
+module.exports = router;
