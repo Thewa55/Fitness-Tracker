eb9b77f2d277aa787b2f328bfa612a4dfc0135e9
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-14 13:59:40 -0400

    Added comments to the index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 init();
 
+//this function looks at the location, if the location does not have anything, it will do an API to retrieve the database.
+//once the call is done, the API.getLastWorkout() will send the last workout back to the URL as the ID so if the user clicks on last workout it will pull up last workout.
+//if there is no last workout, the button is disabled.
 async function init() {
   if (location.search.split("=")[1] === undefined) {
     const workout = await API.getLastWorkout();
