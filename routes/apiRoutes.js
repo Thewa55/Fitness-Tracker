865ac5ada0dabc1d8336354b9abb90e26456937b
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-14 20:00:49 -0400

    Added a get for stats

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,21 @@ const Workout = require("../models/workout.js")
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-    .then(dbWOrkout =>{
-      res.json(dbWOrkout)
+    .then(dbWorkout =>{
+      res.json(dbWorkout)
     })
     .catch( err => {
       res.status(400).json(err)
     })
 })
 
-
+router.get("/api/workouts/range", (req,res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout)  
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
 module.exports = router;
