0cf6d49cdc3d33eeba4fdb0b11dcf764c0a6a4d8
Kenny Lam <kinwai.lam730@gmail.com>
2020-03-14 20:12:09 -0400

    Made a post call

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,20 +4,25 @@ const Workout = require("../models/workout.js")
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then(dbWorkout =>{
-      res.json(dbWorkout)
+      res.json(dbWorkout);
     })
     .catch( err => {
-      res.status(400).json(err)
-    })
-})
+      res.status(400).json(err);
+    });
+});
 
 router.get("/api/workouts/range", (req,res) => {
   Workout.find({})
     .then(dbWorkout => {
-      res.json(dbWorkout)  
+      res.json(dbWorkout);  
     })
     .catch(err => {
-      res.status(400).json(err)
-    })
-})
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts/:id", (req, res)=>{
+  Workout.create
+});
+
 module.exports = router;
